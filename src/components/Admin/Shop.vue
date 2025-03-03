f4fa3dd7f39e0d3b10bab9afb1c2c49c52c01147
tattoo1880 <tattoo186225345941@gmail.com>
2025-03-03 16:04:28 +0800

    123

--- src/components/Admin/Shop.vue
+++ src/components/Admin/Shop.vue
@@ -7,108 +7,71 @@
 
     <!-- 新建店铺按钮 -->
     <el-row class="bg-row" justify="end" style="margin-right: 30px">
-        <el-button
-            type="primary"
-            :icon="CirclePlus"
-            circle
-            @click="prepareCreateShop"
-        />
+        <el-button type="primary" :icon="CirclePlus" circle @click="prepareCreateShop" />
     </el-row>
 
     <!-- 新建店铺dialog -->
     <el-dialog title="新建店铺" v-model="showme1" width="30%" center>
-        <el-form
-            :model="createform"
-            label-position="left"
-            label-width="auto"
-            class="withborder"
-        >
+        <el-form :model="createform" label-position="left" label-width="auto" class="withborder">
             <el-form-item label="店铺名">
                 <el-input v-model="createform.shopname" />
             </el-form-item>
-            <el-form-item label="手机编号">
+            <!-- <el-form-item label="手机编号">
                 <el-input v-model="createform.uid" />
             </el-form-item>
             <el-form-item label="手机地址端口">
                 <el-input v-model="createform.shopip" />
-            </el-form-item>
+            </el-form-item> -->
             <!-- 选择用户 -->
             <el-form-item label="选择用户">
                 <el-select v-model="createform.userid" placeholder="请选择用户">
-                    <el-option
-                        v-for="item in userList"
-                        :key="item.uid"
-                        :label="item.username"
-                        :value="item.uid"
-                    />
+                    <el-option v-for="item in userList" :key="item.uid" :label="item.username" :value="item.uid" ß />
                 </el-select>
             </el-form-item>
 
-            <el-form-item>
+
+            <el-form-item label="店铺key">
+                <el-button type="danger" @click="aagetcookies">
+                    获取key
+                </el-button>
+            </el-form-item>
+
+            <!-- <el-form-item>
                 <el-button type="primary" @click="createShop">新建</el-button>
                 <el-button @click="showme1 = false">取消</el-button>
-            </el-form-item>
+            </el-form-item> -->
         </el-form>
     </el-dialog>
 
     <!-- table -->
     <el-row class="row-bg" justify="center">
         <el-col :span="24">
-            <el-table
-                :data="tabledata"
-                style="width: 100%; height: 70vh"
-                align="center"
-            >
-                <el-table-column
-                    prop="id"
-                    label="ID"
-                    width="100"
-                    align="center"
-                >
+            <el-table :data="tabledata" style="width: 100%; height: 70vh" align="center">
+                <el-table-column prop="id" label="ID" width="100" align="center">
                     <template v-slot="{ row }">
                         <el-tag type="success">{{ row.uid }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column
-                    prop="shopname"
-                    label="店铺名"
-                    width="150"
-                    align="center"
-                >
+                <el-table-column prop="shopname" label="店铺名" width="150" align="center">
                     <template v-slot="{ row }">
                         <el-tag type="success">{{ row.shopName }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column
-                    prop="shopip"
-                    label="店铺地址"
-                    width="200"
-                    align="center"
-                >
+                <el-table-column prop="shopip" label="店铺地址" width="200" align="center">
                     <template v-slot="{ row }">
                         <el-tag type="success">{{ row.shopIp }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column
-                    prop="username"
-                    label="用户名"
-                    width="120"
-                    align="center"
-                >
+                <el-table-column prop="username" label="用户名" width="120" align="center">
                     <template v-slot="{ row }">
                         <el-tag type="success">{{ row.user.username }}</el-tag>
                     </template>
                 </el-table-column>
-                <el-table-column
-                    prop="shopid"
-                    label="到期时间"
-                    width="200"
-                    align="center"
-                >
+                <el-table-column prop="shopid" label="到期时间" width="200" align="center">
                     <template v-slot="{ row }">
                         <el-tag type="success">{{
                             row.user.subEndTime
-                        }}</el-tag>
+                            }}</el-tag>
                     </template>
                 </el-table-column>
                 <!-- <el-table-column
@@ -124,19 +87,10 @@
                         <el-tag v-else type="success">已开启</el-tag>
                     </template>
                 </el-table-column> -->
-                <el-table-column
-                    prop="shopstatus"
-                    label="操作"
-                    width="200"
-                    align="center"
-                >
+                <el-table-column prop="shopstatus" label="操作" width="200" align="center">
                     <template v-slot="{ row }">
-                        <el-button type="primary" @click="openupdateShop(row)"
-                            >编辑</el-button
-                        >
-                        <el-button type="danger" @click="handleDeleteShop(row)"
-                            >删除</el-button
-                        >
+                        <el-button type="primary" @click="openupdateShop(row)">编辑</el-button>
+                        <el-button type="danger" @click="handleDeleteShop(row)">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -145,12 +99,7 @@
 
     <!-- 更新shop -->
     <el-dialog title="更新店铺" v-model="showme2" width="30%" center>
-        <el-form
-            :model="updateform"
-            label-position="left"
-            label-width="auto"
-            class="withborder"
-        >
+        <el-form :model="updateform" label-position="left" label-width="auto" class="withborder">
             <el-form-item label="店铺名">
                 <el-input v-model="updateform.shopname" />
             </el-form-item>
@@ -163,12 +112,7 @@
             <!-- 选择用户 -->
             <el-form-item label="选择用户">
                 <el-select v-model="updateform.userid" placeholder="请选择用户">
-                    <el-option
-                        v-for="item in userList"
-                        :key="item.uid"
-                        :label="item.username"
-                        :value="item.uid"
-                    />
+                    <el-option v-for="item in userList" :key="item.uid" :label="item.username" :value="item.uid" />
                 </el-select>
             </el-form-item>
 
@@ -184,8 +128,8 @@
 import { ref, reactive, onMounted } from "vue";
 import { useShopStore } from "@/stores/shop";
 import { ElMessage } from "element-plus";
-
 import { CirclePlus } from "@element-plus/icons-vue";
+import axios from "axios";
 
 const userList = ref([]);
 const tabledata = ref([]);
@@ -281,6 +225,36 @@ const updateShop = async () => {
         const res2 = await useShopStore().getShopList();
         tabledata.value = res2.data;
     }
+};
+
+
+const aagetcookies = async () => {
+    const res = await axios.post("http://localhost:8080/getcookies",
+        {
+            shopname: createform.shopname,
+            userid: createform.userid,
+        },
+        {
+            timeout: 1000000,
+        }
+
+
+    );
+
+    if (res.data.status === "ok") {
+        ElMessage.success("获取成功");
+        showme1.value = false;
+        const res2 = await useShopStore().getShopList();
+        tabledata.value = res2.data;
+    }
+    else {
+        ElMessage.error("获取失败");
+        showme1.value = false;
+        const res2 = await useShopStore().getShopList();
+        tabledata.value = res2.data;
+    }
+
+
 };
 </script>
 
