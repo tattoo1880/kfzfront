1ea3b1e58ab829ce5f1eaaf4eb5ef07408f0c17d
tattoo1880 <tattoo186225345941@gmail.com>
2025-04-01 15:32:19 +0800

    update

--- src/utils/ApiUrl.js
+++ src/utils/ApiUrl.js
@@ -1,3 +1,4 @@
 //const ApiUrl = "http://185.200.64.73:8083";
-const ApiUrl = "http://127.0.0.1:8083"
+const ApiUrl = "http://212.50.252.74:8083"
+// const ApiUrl = "http://127.0.0.1:8083"
 export default ApiUrl;
