11c9effc6a22f4e03fa560bc395efc56764d5437
tattoo1842 <tattoo@tattoo1842deMacBook-Air.local>
2026-02-11 00:00:15 +0800

    onvps

--- src/utils/GoGinApiUrl.js
+++ src/utils/GoGinApiUrl.js
@@ -1,3 +1,3 @@
-// const GoGinApiUrl = "https://ss.purecode.dpdns.org";
-const GoGinApiUrl = "http://127.0.0.1:8080";
+const GoGinApiUrl = "https://ss.purecode.dpdns.org";
+// const GoGinApiUrl = "http://127.0.0.1:8080";
 export default GoGinApiUrl;
